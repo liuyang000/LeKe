b2593b73400414d48d23d706a7b79b2e666d1cad
yuanye <jl_qiqi@163.com>
2016-07-28 15:33:21 +0800

    修改笔记布局 和代码

---
+++ app/src/main/java/com/rongjie/pdf/adapter/AdaptetFragmentNotes.java
@@ -0,0 +1,98 @@
+package com.rongjie.pdf.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.rongjie.leke.R;
+import com.rongjie.pdf.bean.NoteInfo;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2016/7/28.
+ * 笔记适配器
+ */
+public class AdaptetFragmentNotes extends RecyclerView.Adapter<AdaptetFragmentNotes.HolerFragmentNotes> {
+
+    private Context mContext;
+    private List<NoteInfo> mInfos;
+
+    public AdaptetFragmentNotes(Context context, List<NoteInfo> noteInfos) {
+        this.mContext = context;
+        this.mInfos = noteInfos;
+    }
+
+    @Override
+    public HolerFragmentNotes onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = View.inflate(mContext, R.layout.adapter_fragment_notes, null);
+        return new HolerFragmentNotes(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final HolerFragmentNotes holder, int position) {
+        holder.mRlItemNotes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mListener != null)
+                    mListener.onClickItemNotesListener(mInfos.get(holder.getLayoutPosition()));
+            }
+        });
+
+        holder.setViewData(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mInfos != null) {
+            return mInfos.size();
+        }
+        return 0;
+    }
+
+    //holder band
+    public class HolerFragmentNotes extends RecyclerView.ViewHolder {
+
+        private final TextView mTvBookName;
+        private final RelativeLayout mRlItemNotes;
+
+        public HolerFragmentNotes(View itemView) {
+            super(itemView);
+            mTvBookName = (TextView) itemView.findViewById(R.id.tv_book_name);
+            mRlItemNotes = (RelativeLayout) itemView.findViewById(R.id.rl_item_book);
+        }
+
+        public void setViewData(int position) {
+            if (mInfos != null && mInfos.size() > 0) {
+                //设置书的名字
+                if (mInfos.get(position).isAddView()){
+                    mTvBookName.setText("ADD");
+                }else{
+                    mTvBookName.setText(mInfos.get(position).getFileName());
+                }
+            }
+        }
+    }
+
+
+    private ClickItemNotesListener mListener;
+
+    /**
+     * 书籍  点击事件的监听
+     */
+    public interface ClickItemNotesListener {
+
+        void onClickItemNotesListener(NoteInfo info);
+    }
+
+    /**
+     * 点击事件
+     */
+    public void setListener(ClickItemNotesListener listener) {
+        this.mListener = listener;
+    }
+}
--- app/src/main/java/com/rongjie/pdf/fragment/NotesFragment.java
+++ app/src/main/java/com/rongjie/pdf/fragment/NotesFragment.java
@@ -1,25 +1,175 @@
 package com.rongjie.pdf.fragment;
 
 import android.app.Fragment;
+import android.icu.text.IDNA;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.rongjie.leke.MyApplication;
 import com.rongjie.leke.R;
+import com.rongjie.pdf.View.dialog.BookMarksDialog;
+import com.rongjie.pdf.adapter.AdaptetFragmentNotes;
+import com.rongjie.pdf.bean.NoteInfo;
+import com.rongjie.pdf.utils.FileUtils;
+import com.rongjie.pdf.utils.StringUtils;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
  * Created by Administrator on 2016/7/12.
  * 笔记
  */
-public class NotesFragment extends Fragment {
-    private ViewGroup mRootView;
+public class NotesFragment extends Fragment implements AdaptetFragmentNotes.ClickItemNotesListener, BookMarksDialog.DialogClickFinsihListener {
 
-    @Nullable
+    private List<NoteInfo> mNotes = new ArrayList<NoteInfo>();
+    private ViewGroup mRootView;
+    private RecyclerView mRecyclerView;
+    private AdaptetFragmentNotes mAdaptetFragmentNotes;
+    private ProgressBar mProgressBar;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return mRootView = (ViewGroup) inflater.inflate(R.layout.fragment_notes, null);
+        mRootView = (ViewGroup) inflater.inflate(R.layout.fragment_notes, null);
+        mProgressBar = (ProgressBar) mRootView.findViewById(R.id.progressBar);
+        mProgressBar.setVisibility(View.VISIBLE);
+
+        //做了一个延迟100 毫秒的操作 ，主要是为了解决，系统在遍历文件是需要时间的，进来会黑屏一段才显示
+        //先加载布局然后在做遍历，用户体检会解决很好
+
+        MyApplication.getMainThreadHandler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                initBookDatas();
+                findBooks();
+                mAdaptetFragmentNotes.notifyDataSetChanged();
+                mProgressBar.setVisibility(View.GONE);
+            }
+        }, 100);
+
+        return mRootView;
     }
+
+    /**
+     * 初始化 书列表
+     */
+    private void initBookDatas() {
+        mRecyclerView = (RecyclerView) mRootView.findViewById(R.id.recycler_View);
+        GridLayoutManager layout = new GridLayoutManager(getActivity(), 4);
+        mRecyclerView.setLayoutManager(layout);
+        mAdaptetFragmentNotes = new AdaptetFragmentNotes(getActivity(),mNotes);
+        mAdaptetFragmentNotes.setListener(this);
+        mRecyclerView.setAdapter(mAdaptetFragmentNotes);
+        mRecyclerView.setHasFixedSize(true);
+    }
+
+    /**
+     * 搜索本地的书
+     */
+    private void findBooks() {
+        mNotes.clear();
+        String bookFile = FileUtils.getTextBookFilesDir(getActivity());
+        System.out.println("appFile ==" + bookFile);
+        File dir = new File(bookFile);
+        File[] files = dir.listFiles();
+        if (files == null || files.length == 0) {
+            return;
+        }
+        for (int i = 0; i < files.length; i++) {
+            File file = files[i];
+            if (!file.isDirectory() && file.getName().contains("pdf")) {
+                System.out.println("  file.getName()" + file.getName());
+                NoteInfo info = new NoteInfo();
+                info.setFileName( file.getName().substring(0, file.getName().length() - 4));
+                mNotes.add(info);
+            }
+
+        }
+        NoteInfo info = new NoteInfo();
+        info.setAddView(true);
+        mNotes.add(info);
+        dir = null;
+        files = null;
+        System.gc();
+        System.out.println("books.size" + mNotes.size());
+    }
+
+
+
+    @Override
+    public void onHiddenChanged(boolean hidden) {
+        super.onHiddenChanged(hidden);
+        // 不需要做什么处理
+        System.out.println("onHiddenChanged == " + hidden);
+    }
+
+    @Override
+    public void onClickItemNotesListener(NoteInfo info) {
+        if (info.isAddView()){
+            //TODO:弹出框
+            Toast.makeText(getActivity(),"ADD"+"：笔记", Toast.LENGTH_SHORT).show();
+            showBookMarksDialog("add",BookMarksDialog.DialogMode.ADD,"添加笔记本");
+        }else{
+            //TODO:进入笔记
+            Toast.makeText(getActivity(), info.getFileName()+"：笔记", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
+   private BookMarksDialog mBookMarkDialog ;
+
+    public void showBookMarksDialog(String tag, BookMarksDialog.DialogMode dialogMode, String msg) {
+
+        if (null == mBookMarkDialog) {
+            mBookMarkDialog = new BookMarksDialog(getActivity());
+            mBookMarkDialog.setListener(NotesFragment.this);
+        }
+
+        if (!mBookMarkDialog.isShowing()) {
+            mBookMarkDialog.show();
+            mBookMarkDialog.setTag(tag);
+            mBookMarkDialog.setDialogMode(dialogMode);
+            mBookMarkDialog.setEditBookMarksContent(msg);
+        }
+    }
+
+    //dialog点击按钮
+    @Override
+    public void onDialogConfirmCancleClick(BookMarksDialog dialog, BookMarksDialog.ClickState clickState) {
+
+        if (clickState == BookMarksDialog.ClickState.IMG_BTN_CLOSE_CLICKED) {
+            System.out.println("IMG_BTN_CLOSE_CLICKED");
+
+        } else if (clickState == BookMarksDialog.ClickState.BTN_CHANGE_CLICKED) {
+        } else if (clickState == BookMarksDialog.ClickState.BTN_DELETE_CLICKED) {
+        } else if (clickState == BookMarksDialog.ClickState.BTN_ADD_CLICKED) {
+            //TODO:添加笔记
+            String fileName = dialog.getEditBookMarksContent();
+            if (StringUtils.isEmpty(fileName)){
+                return;
+            }else {
+                NoteInfo addView = mNotes.remove(mNotes.size() - 1);
+                NoteInfo info = new NoteInfo();
+                info.setFileName(fileName);
+                info.setAddInfo(true);
+                mNotes.add(info) ;
+                mNotes.add(addView);
+            }
+            mAdaptetFragmentNotes.notifyDataSetChanged();
+        }
+
+        dialog.dismiss();
+    }
+
+
 }
